c8031ae154b64078f8da62df79f0bd99d9856d1e
tamagooc <tamagomago6212@gmail.com>
2024-09-20 04:50:26 +0000

    9月19日分を別ファイルに移動

---
+++ main.js
@@ -0,0 +1,21 @@
+document.querySelector("h1").textContent = "nishiuchi"
+
+document.querySelector("h2").textContent = "nishiuchi"
+
+//documentとは!!
+/* htmlファイルで書かれているコードを一つののオブジェクトとしてみなす。 */
+
+//DOM(ドム)
+/* 
+    ドキュメントオブジェクトモデル (Document Object Model）の略で、Webページの要素やコンテンツなどを
+    ツリー構造で表現したデータモデル
+*/
+
+//上記コードの意味
+//document.querySelector("h1").textContent = "自分の名前"
+
+//ドキュメントの中の("h1")のタグを探してね
+//document.querySelector("h1")
+
+//h1タグの中身のtextを変えてね
+//.textContent = "自分の名前"
