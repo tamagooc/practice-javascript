d9f0534beb0363331d2e1e35559f5b73a7372d02
tamagooc <tamagomago6212@gmail.com>
2024-10-04 05:57:50 +0000

    10_4写経

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 //クリックされたら動作する
 function errorInput(){
-    console.log(document.getElementById("month").value)
-    console.log(document.getElementById("day").value)
-
     const month = document.getElementById("month").value;
     const day = document.getElementById("day").value;
+
+    if(month<1 || month>12){
+        alert("適切な値ではありません");
+        return
+    }
 
     console.log("month:", month, "day:", day)
 
 }
+
+
 
 //document.getElementById("month")はタグを確認するだけ
 //valueはそのタグの値を示す
